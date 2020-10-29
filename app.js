c8106bd7f86cddf95f89a450be9964dc53f59135
Amber11-hub <73658756+Amber11-hub@users.noreply.github.com>
2020-10-29 21:18:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+ 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
